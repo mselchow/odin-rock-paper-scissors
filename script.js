f73550e611ee6eb14bc36397336a1e99570291a6
Michael Selchow <mselchow@pm.me>
2022-03-14 22:46:07 -0400

    Add computerPlay function with random return

---
+++ script.js
@@ -0,0 +1,16 @@
+function computerPlay() {
+    switch (Math.floor(Math.random() * 3)) {
+        case 0:
+            return "Rock";
+            break;
+        case 1:
+            return "Paper";
+            break;
+        case 2:
+            return "Scissors";
+            break;
+        default:
+            console.log("computerPlay returned an unexpected value.");
+            break;
+    }
+}
